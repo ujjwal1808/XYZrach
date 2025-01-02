6458e0c9974e080fd1120a9af3f41242a8f9cab0
The-3rio <craftmenlab@gmail.com>
2025-01-02 22:51:17 +0530

    fixed

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -7,66 +7,66 @@ const Cards = () => {
 
     return (
         <section className="mainn section1">
-            <div class="card blue">
+            <div className="card blue">
                 
-                <div class="card-body">
+                <div className="card-body">
                     <h3>Mobile App Development</h3>
                     <p>Transform ideas into reality with our Mobile App Development services. </p>
                     
                 </div>
-                <div class="card-footer ">
-                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} class="btn-countdown">Read more</Link>
+                <div className="card-footer ">
+                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} className="btn-countdown">Read more</Link>
                 </div>
             </div>
-            <div class="card blue">
+            <div className="card blue">
                 
-                <div class="card-body">
+                <div className="card-body">
                     <h3>Web application development</h3>
                     <p>Empower your business with our Web Application Development services.</p>
                     
                 </div>
-                <div class="card-footer">
+                <div className="card-footer">
                     
 
-                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} class="btn-countdown">Read more</Link>
+                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} className="btn-countdown">Read more</Link>
                 </div>
             </div>
-            <div class="card blue">
+            <div className="card blue">
                 
-                <div class="card-body">
+                <div className="card-body">
                     <h3>UI UX design</h3>
                     <p>Enhance user satisfaction with our UI/UX Design services. </p>
                     
                 </div>
-                <div class="card-footer">
+                <div className="card-footer">
                     
-                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} class="btn-countdown">Read more</Link>
+                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} className="btn-countdown">Read more</Link>
                 </div>
             </div>
-            <div class="card blue">
+            <div className="card blue">
                 
-                <div class="card-body">
+                <div className="card-body">
                     <h3>Intergration
                     services</h3>
                     <p>Seamlessly connect your systems with our Integration Services.</p>
                     
                 </div>
-                <div class="card-footer">
+                <div className="card-footer">
                     
 
-                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} class="btn-countdown">Read more</Link>
+                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} className="btn-countdown">Read more</Link>
                 </div>
             </div>
-            <div class="card blue">
+            <div className="card blue">
                 
-                <div class="card-body">
+                <div className="card-body">
                     <h3>Staff Augmentation
                     </h3>
                     <p>Scale your team effortlessly with our Staff Augmentation services. </p>
                     
                 </div>
-                <div class="card-footer">
-                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} class="btn-countdown">Read more</Link>
+                <div className="card-footer">
+                    <Link to="/services" onClick={() => { window.scroll(0, 0); }} className="btn-countdown">Read more</Link>
                 </div>
             </div>
             
